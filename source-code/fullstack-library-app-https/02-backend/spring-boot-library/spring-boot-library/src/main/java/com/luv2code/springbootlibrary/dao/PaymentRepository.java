ba99caf7ae5f07abd6b7553942e73558103c4b0c
Kevin <kevin@Kevins-MBP.hsd1.md.comcast.net>
2024-06-06 13:50:50 -0400

    Update readmes to reflect me

--- source-code/fullstack-library-app-https/02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.KBUCK2018.springbootlibrary.dao;
-
-import com.KBUCK2018.springbootlibrary.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-    Payment findByUserEmail(String userEmail);
-}
